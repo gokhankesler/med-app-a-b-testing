13f2567b028edbbcd5f2525128ec47a456afd7dd
Gokhan KESLER <gokhankesler@gmail.com>
2021-11-22 15:57:07 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#from distutils.core import setup
+import re
+from setuptools import setup, find_packages
+
+
+setup(name='Med App A/B Testing',
+      version='0.1.0',
+      description='''This project implements A/B testing of a mobile app that offers 
+      meditation services  for a paid subscription as well as one-off in-app purchases''',
+      license="MIT",
+      author='Gokhan Kesler',
+      author_email='gokhankesler@gmail.com',
+      packages=find_packages(include=["src"]),
+     )
